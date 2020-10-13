46ea1bbceb13372338249c9ed70ee7c5bcce536e
David Ramalho <david.m.ramalho@gmail.com>
2020-10-13 22:07:22 +0000

    exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,29 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validate :first_name_present, :last_name_present, :employees_must_belong_to_store, :hourly_rate_value_integer
+
+  def first_name_present
+    if !first_name.present?
+        errors.add(:first_name, "Must have a first name!")
+    end
+  end
+
+  def last_name_present
+      if !last_name.present?
+          errors.add(:last_name, "Must have a last name!")
+      end
+  end
+
+  def employees_must_belong_to_store
+      if !store_id.present?
+          errors.add(:store_id, "Employee must belong to store.")
+      end
+  end
+
+  def hourly_rate_value_integer
+      if hourly_rate.to_i && (hourly_rate < 40 || hourly_rate > 200)
+          errors.add(:hourly_rate, "Hourly rate must be an integer from 40 to 200")
+      end
+  end
 end
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3, message: 'Must have a minimum of 3 characters for store names' }
+  validates :annual_revenue, numericality:
+  { only_integer: true, greater_than: 0, message: 'Must have an annual revenue greater than 0' }
+  validate :store_must_womens_or_mens
+
+  def store_must_womens_or_mens
+      if !womens_apparel.present? && !mens_apparel.present?
+          errors.add(:store_must_womens_or_mens, 'Stores must carry at least mens or womens apparel.')
+      end
+  end
 end
